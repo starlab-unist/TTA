0359806e608cb49bc4d17e474676b60f26dfb294
류병우 <captainnemo9292@gmail.com>
2025-11-23 19:35:11 +0900

    Update dev_test.sh

--- test_generation/dev_test.sh
+++ test_generation/dev_test.sh
@@ -1,11 +1,11 @@
 mkdir /root/eval/dev
 
 cd /root/projects/bluebird
-npx istanbul cover tools/test.js -- --expose-gc > /root/eval/dev/coverage_bluebird.log
+npx istanbul cover tools/test.js --expose-gc > /root/eval/dev/coverage_bluebird.log
 
 cd /root/projects/q
 npm install --save-dev nyc
-npx nyc jasmine-node spec && nyc report --reporter=html && opener coverage/index.html > /root/eval/dev/coverage_q.log
+npx nyc jasmine-node spec && nyc report --reporter=html > /root/eval/dev/coverage_q.log
 
 cd /root/projects/Complex.js
 npx nyc mocha tests/*.js > /root/eval/dev/coverage_complex.js.log

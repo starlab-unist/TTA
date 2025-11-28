12ae076d9d7d9c1e4ac1467bb8e904e553413a07
류병우 <captainnemo9292@gmail.com>
2025-11-28 18:56:00 +0900

    Update gen_test.sh

--- test_generation/gen_test.sh
+++ test_generation/gen_test.sh
@@ -1,44 +1,62 @@
+ROOT_DIR="/home/starlab1/TTA/test_generation"
+
 # env setup
 
-source test-env/bin/activate
+source "$ROOT_DIR/test-env/bin/activate"
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm use 16
 
 # extract APIs for each project
 
-cd /root
-mkdir api
+cd "$ROOT_DIR"
+mkdir "$ROOT_DIR/api"
 
-node exploreAPI.js /root/projects/bluebird /root/api/bluebird_api.json
-node exploreAPI.js /root/projects/q /root/api/q_api.json
-node exploreAPI.js /root/projects/pull-stream /root/api/pull-stream_api.json
-node exploreAPI.js /root/projects/Complex.js /root/api/complex.js.json
-node exploreAPI.js /root/projects/memfs /root/api/memfs_api.json
+node exploreAPI.js "$ROOT_DIR/projects/bluebird" "$ROOT_DIR/api/bluebird_api.json"
+node exploreAPI.js "$ROOT_DIR/projects/q" "$ROOT_DIR/api/q_api.json"
+node exploreAPI.js "$ROOT_DIR/projects/pull-stream" "$ROOT_DIR/api/pull-stream_api.json"
+node exploreAPI.js "$ROOT_DIR/projects/Complex.js" "$ROOT_DIR/api/complex.js.json"
+node exploreAPI.js "$ROOT_DIR/projects/memfs" "$ROOT_DIR/api/memfs_api.json"
 
 
 # run experiment
 
-cd /root
+cd "$ROOT_DIR"
+mkdir "$ROOT_DIR/gen_test"
 
-mkdir /root/gen_test/bluebird
-mkdir /root/gen_test/q
-mkdir /root/gen_test/complex.js
-mkdir /root/gen_test/pull-stream
-mkdir /root/gen_test/memfs
+mkdir "$ROOT_DIR/gen_test/bluebird"
+mkdir "$ROOT_DIR/gen_test/q"
+mkdir "$ROOT_DIR/gen_test/complex.js"
+mkdir "$ROOT_DIR/gen_test/pull-stream"
+mkdir "$ROOT_DIR/gen_test/memfs"
 
-python3 test_gen.py bluebird /root/api/bluebird_api.json
-python3 test_gen.py q /root/api/q_api.json
-python3 test_gen.py complex.js /root/api/complex.js_api.json
-python3 test_gen.py pull-stream /root/api/pull-stream_api.json
-python3 test_gen.py memfs /root/api/memfs_api.json
+python3 test_gen.py bluebird "$ROOT_DIR/api/bluebird_api.json"
+python3 test_gen.py q "$ROOT_DIR/api/q_api.json"
+python3 test_gen.py complex.js "$ROOT_DIR/api/complex.js.json"
+python3 test_gen.py pull-stream "$ROOT_DIR/api/pull-stream_api.json"
+python3 test_gen.py memfs "$ROOT_DIR/api/memfs_api.json"
 
 
 # run coverage
 
-mkdir /root/eval
-mkdir /root/eval/coverage
+mkdir "$ROOT_DIR/eval"
+mkdir "$ROOT_DIR/eval/coverage"
 
-npm run test:eval /root/gen_test/bluebird/unit/*.js /root/gen_test/bluebird/branch/*.js > /root/eval/coverage/coverage_bluebird.log
-npm run test:eval /root/gen_test/q/unit/*.js /root/gen_test/q/branch/*.js > /root/eval/coverage/coverage_q.log
-npm run test:eval /root/gen_test/complex.js/unit/*.js /root/gen_test/complex.js/branch/*.js > /root/eval/coverage/coverage_complex.js.log
-npm run test:eval /root/gen_test/pull-stream/unit/*.js /root/gen_test/pull-stream/branch/*.js > /root/eval/coverage/coverage_pull-stream.log
-npm run test:eval /root/gen_test/memfs/unit/*.js /root/gen_test/memfs/branch/*.js > /root/eval/coverage/coverage_memfs.log
+
+cd "$ROOT_DIR/projects/bluebird"
+npm run prepublish
+cd "$ROOT_DIR"
+npm run test:eval "$ROOT_DIR/gen_test/bluebird/unit/*.js" "$ROOT_DIR/gen_test/bluebird/branch/*.js" > "$ROOT_DIR/eval/coverage/coverage_bluebird.log"
+
+cd "$ROOT_DIR/projects/q"
+npm run prepublish
+cd "$ROOT_DIR"
+npm run test:eval "$ROOT_DIR/gen_test/q/unit/*.js" "$ROOT_DIR/gen_test/q/branch/*.js" > "$ROOT_DIR/eval/coverage/coverage_q.log"
+
+
+npm run test:eval "$ROOT_DIR/gen_test/complex.js/unit/*.js" "$ROOT_DIR/gen_test/complex.js/branch/*.js" > "$ROOT_DIR/eval/coverage/coverage_complex.js.log"
+
+npm run test:eval "$ROOT_DIR/gen_test/pull-stream/unit/*.js" "$ROOT_DIR/gen_test/pull-stream/branch/*.js" > "$ROOT_DIR/eval/coverage/coverage_pull-stream.log"
+
+npm run test:eval "$ROOT_DIR/gen_test/memfs/unit/*.js" "$ROOT_DIR/gen_test/memfs/branch/*.js" > "$ROOT_DIR/eval/coverage/coverage_memfs.log"

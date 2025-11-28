affa7dd015319f573ec6ec3f736004c75bef5a7a
류병우 <captainnemo9292@gmail.com>
2025-11-28 18:55:33 +0900

    Update setup.sh

--- test_generation/setup.sh
+++ test_generation/setup.sh
@@ -1,10 +1,11 @@
+ROOT_DIR="/home/starlab1/TTA/test_generation"
+
 # python env setup
 
 python3 -m venv test-env
 source test-env/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
-
 
 # node env setup
 
@@ -15,66 +16,51 @@ nvm install 16
 nvm use 16
 nvm alias default 16
 
-# ollama install
-
-curl -fsSL https://ollama.com/install.sh | sh
-nohup ollama serve > ollama.log &
-echo "Waiting for Ollama to start..."
-for i in {1..10}; do
-  if curl -s http://localhost:11434 > /dev/null; then
-    echo "Ollama is up!"
-    break
-  fi
-  sleep 1
-done
-ollama pull qwen2.5:72b
-
 # collect & install projects
 
 mkdir projects
 
-cd /root/projects
+cd "$ROOT_DIR/projects"
 git clone https://github.com/petkaantonov/bluebird.git
-cd /root/projects/bluebird
+cd "$ROOT_DIR/projects/bluebird"
 git checkout 6c8c069
 
 npm install
 npm run prepublish
 
-cd /root/projects
+cd "$ROOT_DIR/projects"
 git clone https://github.com/kriskowal/q.git
-cd /root/projects/q
+cd "$ROOT_DIR/projects/q"
 git checkout 6bc7f52
 
 npm install
 npm run prepublish
 
 
-cd /root/projects
+cd "$ROOT_DIR/projects"
 git clone https://github.com/rawify/Complex.js.git
-cd /root/projects/Complex.js
+cd "$ROOT_DIR/projects/Complex.js"
 git checkout d995ca1
 
 npm install
 
 
-cd /root/projects/
+cd "$ROOT_DIR/projects"
 git clone https://github.com/pull-stream/pull-stream.git
-cd /root/projects/pull-stream
+cd "$ROOT_DIR/projects/pull-stream"
 git checkout 29b4868
 
 npm install
 
 
-cd /root/projects/
+cd "$ROOT_DIR/projects"
 git clone https://github.com/streamich/memfs.git
-cd /root/projects/memfs
+cd "$ROOT_DIR/projects/memfs"
 git checkout ec83e6f
 
 npm install --legacy-peer-deps
 npm run build
 
 
-cd /root
+cd "$ROOT_DIR"
 npm install
-
